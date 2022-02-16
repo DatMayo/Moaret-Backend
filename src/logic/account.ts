eb1e4fc644327d7368ec387fa46c4f006daf6a0b
Mario Franze <mfranze@codingden.de>
2022-02-16 06:45:53 +0100

    Added some account logic

--- src/logic/account.ts
+++ src/logic/account.ts
@@ -1,15 +1,57 @@
+import { Connection, getConnection, Repository } from "typeorm";
+import { DBUser } from "../db/entities";
+import { compareSync, hashSync } from "bcryptjs";
+
 export class Account {
-  readonly _username: string;
+  private _connection: Connection;
+  private _repositoryUser: Repository<DBUser>;
 
-  constructor(username: string) {
-    this._username = username;
+  constructor() {
+    this._connection = getConnection();
+    this._repositoryUser = this._connection.getRepository(DBUser);
   }
 
-  login(password: string) {
-    // ToDo: Login magic
+  async create(username: string, password: string, mail: string) {
+    const user: DBUser = new DBUser();
+    user.username = username;
+    user.password = hashSync(password);
+    user.mail = mail;
+    const result = await this._repositoryUser.save(user);
+    return result;
   }
 
-  register(password: string) {
-    // ToDo: Register magic
+  async getAll() {
+    return await this._repositoryUser.find({
+      select: ["id", "username", "mail"],
+    });
+  }
+
+  async getById(userId: number) {
+    return await this._repositoryUser.findOne({
+      select: ["id", "username", "mail"],
+      where: {
+        id: userId,
+      },
+    });
+  }
+
+  async getByUsername(user: string) {
+    return await this._repositoryUser.findOne({
+      select: ["id", "username", "mail"],
+      where: {
+        username: user,
+      },
+    });
+  }
+
+  async login(user: string, pwd: string) {
+    const userHandle = await this._repositoryUser.findOne({
+      select: ["id", "username", "mail", "password"],
+      where: {
+        username: user,
+      },
+    });
+    if (!userHandle) return false;
+    return compareSync(pwd, userHandle.password);
   }
 }
