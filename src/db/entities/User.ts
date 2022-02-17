77d5e4b9a9aaca2ed7f280221179116c072a65b0
Mario Franze <mfranze@codingden.de>
2022-02-17 20:05:03 +0100

    Added more logic

--- src/db/entities/User.ts
+++ src/db/entities/User.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { DBBaseEntity } from "./DBBaseEntity";
+import { DBCompany } from "./Company";
 
 @Entity()
 export class DBUser extends DBBaseEntity {
@@ -9,9 +16,22 @@ export class DBUser extends DBBaseEntity {
   @Column()
   username: string;
 
-  @Column()
+  @Column({ select: false })
   password: string;
 
   @Column()
   mail: string;
+
+  @Column({ default: true })
+  active: boolean;
+
+  @Column({ nullable: true })
+  firstName?: string;
+
+  @Column({ nullable: true })
+  lastName?: string;
+
+  @OneToMany(() => DBCompany, (company) => company.owner)
+  @JoinColumn()
+  companies?: DBCompany[];
 }
